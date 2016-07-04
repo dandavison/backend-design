819f04708dd9bcf29cf95731612704603a855780
Dan Davison <dandavison7@gmail.com>
2016-07-03 22:05:40 -0700

    Compute public attributes

--- backend_design/graph.py
+++ backend_design/graph.py
@@ -1,7 +1,10 @@
+import inspect
+
 from backend_design.utils import classproperty
 
 
 class _Node(object):
+    _private_attribute_names = set()
 
     def __init__(self, *args, **kwargs):
         raise TypeError("Node subclasses serve as singletons and "
@@ -17,9 +20,26 @@ class _Node(object):
 
     @classmethod
     def get_attributes(cls):
-        return {
+        attrs = {
             'label': cls.label,
         }
+        attrs.update({
+            key: value
+            for key, value in vars(cls).iteritems()
+            if cls.is_public_data_attribute(key, value)
+        })
+        return attrs
+
+    @classmethod
+    def is_public_data_attribute(cls, name, value):
+        if name in cls._private_attribute_names:
+            return False
+        elif name.startswith('_'):
+            return False
+        elif inspect.ismethoddescriptor(value):
+            return False
+        else:
+            return True
 
     @classmethod
     def add_to_graph(cls, graph):
@@ -35,6 +55,8 @@ class State(_Node):
 class Event(_Node):
     from_state = None
     to_state = None
+
+    _private_attribute_names = {'from_state', 'to_state'}
 
     @classmethod
     def get_edge_data(cls):
